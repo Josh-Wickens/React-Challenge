a507c494c935157e62e0de5d2ad1ccf438928723
Josh-Wickens <joshwickens@hotmail.com>
2022-11-28 21:51:50 +0000

    add stateful greeting and test on click state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
+import StatefulGreeting from './components/StatefulGreeting';
+
 
 function App() {
   return (
     <div className={css.App}>
-      <Sidebar />
+      <StatefulGreeting greeting="i'm a stateful class component!"/>
+
     </div>
   );
 }
